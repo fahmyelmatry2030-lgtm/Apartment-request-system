c1e8016f1eba52d7c34dc8c8da5dde3c0880dd2c
Fahmyelmatry <fahmyelmatry2030@gmail.com>
2026-08-09 19:52:38 +0300

    Fix videoSrc path to point to existing video file

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -71,7 +71,7 @@ const EXTERNAL_APARTMENTS = [
       '/images/apt1/apt1-15.jpg',
       '/images/apt1/apt1-16.jpg',
     ],
-    videoSrc: '/images/apt1/apt1-video.mp4',
+    videoSrc: '/images/video/studio 1.mp4',
     browseHref: '/mazar/units/apt-1',
     unitId: 'apt-1',
   },
